a2ffc3c259daafe14e68a28f8d80294609649223
adamp <adam.piskorek@gmx.de>
2025-10-01 20:36:39 +0200

    hero data added

--- src/app/interfaces/data.ts
+++ src/app/interfaces/data.ts
@@ -6,5 +6,12 @@ header: {
         skillSet: {set: boolean, values: string[]}; 
         myWork: {set: boolean, values: string[]};
         };
-    
+hero: {
+        photo: {xPosition: number, width: number};
+        hello: {xPosition: number, width: number, values: string[]};
+        profesion: {xPosition: number, width: number, values: string[]};
+        scroll: {xPosition: number, width: number, values: string[]};
+        scrollline: {xPosition: number, width: number};
+        socialButtons: {xPosition: number, width: number};
+        };
 }
--- src/app/services/data.ts
+++ src/app/services/data.ts
@@ -13,6 +13,14 @@ export class DATA {
       aboutMe: { set: false, values: ['About me', 'Über mich'] },
       skillSet: { set: false, values: ['Skill set', 'Fähigkeiten'] },
       myWork: { set: false, values: ['My work', 'Projekt'] },
+    },
+    hero: {
+      photo: { xPosition: 0, width: 645},
+      hello: { xPosition: 731, width: 645, values: ['Hello! I am Adam', 'Hallo! Ich bin Adam, ein Frontend Entwickler.'] },
+      profesion: { xPosition: 731, width: 645, values: ['Frontend & Backend Developer', 'Frontend und Backend Entwickler'] },
+      scroll: { xPosition: 731, width: 645, values: ['Scroll down', 'Runterscrollen'] },
+      scrollline: { xPosition: 731, width: 645 },
+      socialButtons: { xPosition: 731, width: 645 }
     }
-  }
+  };
 }
