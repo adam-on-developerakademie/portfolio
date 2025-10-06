e6576166a9d50440c85f36c4afa1b5d6807e3161
adamp <adam.piskorek@gmx.de>
2025-10-06 23:42:48 +0200

    ball 2 and 3 added

--- src/app/portfolio/portfolio.ts
+++ src/app/portfolio/portfolio.ts
@@ -2,14 +2,12 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DATA } from '../../services/data';
 import { Header } from "../header/header";
-import { Hero } from './hero/hero';
-import { AboutMe } from '../portfolio/about-me/about-me';
 //import { SkillSet } from '../skill-set/skill-set';
 //import { MyWork } from '../my-work/my-work';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [CommonModule, Header, Hero, AboutMe //, SkillSet, MyWork
+  imports: [CommonModule, Header //, SkillSet, MyWork
 
   ],
   templateUrl: './portfolio.html',
